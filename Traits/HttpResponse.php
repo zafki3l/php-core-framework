6814ff8e45249937cbc28a8d4e687f8e9f135405
zafki3l <phamdinhthai2005@gmail.com>
2025-10-29 23:38:38 +0700

    add comments for Traits Classes

--- Traits/HttpResponse.php
+++ Traits/HttpResponse.php
@@ -2,16 +2,30 @@
 
 namespace Traits;
 
+/**
+ * Trait HttpResponseTrait
+ * 
+ * Provides helper methods for handling HTTP redirects
+ */
 trait HttpResponseTrait
 {
-    // Redirect to the specified path
+    /**
+     * Redirect to the specified path
+     * 
+     * @param string $path
+     * @return never
+     */
     public function redirect(string $path) : void
     {
         header('Location: /' . PROJECT_NAME . $path);
         exit();
     }
 
-    // Redirect to the previous path
+    /**
+     * Redirect to the previous path
+     * 
+     * @return never
+     */
     public function back() : void
     {
         header('Location: ' . $_SERVER['HTTP_REFERER']);
--- Traits/ModelTrait.php
+++ Traits/ModelTrait.php
@@ -2,13 +2,37 @@
 
 namespace Traits;
 
+/**
+ * Trait ModelTrait
+ * 
+ * Provides helper methods for Model classes
+ */
 trait ModelTrait
 {
+    /**
+     * Magic getters
+     * 
+     * A better solution for regular getters
+     * Dynamically retrieves the value of an existing property.
+     * 
+     * @param mixed $name
+     * @return mixed
+     */
     public function __get(mixed $name) : mixed
     {
         return property_exists($this, $name) ? $this->$name : null;
     }
 
+    /**
+     * Magic setters
+     * 
+     * A better solution for regular setters
+     * Dynamically assigns a value to an existing property.
+     * 
+     * @param mixed $name
+     * @param mixed $value
+     * @return void
+     */
     public function __set($name, $value) : void
     {
         if (property_exists($this, $name)) {
@@ -16,6 +40,12 @@ trait ModelTrait
         }
     }
 
+    /**
+     * Automatically binds associative array data to the model's properties
+     * 
+     * @param array $data
+     * @return void
+     */
     public function fill(array $data) : void
     {
         foreach ($data as $key => $value) {
