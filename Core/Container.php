2a32ae25e9d0c1fcb9defc8bd3e60fcc03a9544d
zafki3l <phamdinhthai2005@gmail.com>
2025-10-30 00:07:26 +0700

    add comments for Container

--- Core/Container.php
+++ Core/Container.php
@@ -2,18 +2,44 @@
 
 namespace Core;
 
-use Exception;
 use ReflectionClass;
 
+/**
+ * Class Container
+ * 
+ * Responsible for binding and resolving class dependencies
+ */
 class Container
 {
+    /**
+     * The registered bindings
+     * 
+     * @var array
+     */
     protected array $bindings = [];
 
+    /**
+     * Bind a class or interface to a resolver callback
+     * 
+     * @param string $class
+     * @param callable $callback
+     * @return void
+     */
     public function bind(string $class, callable $callback): void
     {
         $this->bindings[$class] = $callback;
     }
 
+    /**
+     * Resolve an instance of the given class.
+     *
+     * If a binding exists, it uses the custom resolver.
+     * Otherwise, it attempts to instantiate the class automatically
+     * by resolving constructor dependencies using reflection.
+     * 
+     * @param string $class
+     * @return object
+     */
     public function resolve(string $class) : object
     {
         if (isset($this->bindings[$class])) {
