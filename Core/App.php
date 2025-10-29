a71d0dd0b39d49926f5a12ee4e7941fc4010419e
zafki3l <phamdinhthai2005@gmail.com>
2025-10-30 00:11:25 +0700

    add comment for App

--- Core/App.php
+++ Core/App.php
@@ -2,20 +2,45 @@
 
 namespace Core;
 
+/**
+ * Class App
+ * 
+ * This class serves as the main container for the application, managing dependency injection
+ * and service resolution throughout the application lifecycle. It provides a centralized
+ * way to handle service container functionality and dependency management.
+ * 
+ */
 class App
 {
     protected static Container $container;
 
+    /**
+     * Set the application's container instance
+     * 
+     * @param \Core\Container $container
+     * @return void
+     */
     public static function setContainer(Container $container) : void
     {
         static::$container = $container;
     }
 
+    /**
+     * Resolve a class instance through the container
+     * 
+     * @param string $class
+     * @return object
+     */
     public static function resolve(string $class) : object
     {
         return static::container()->resolve($class);
     }
 
+    /**
+     * Get the current container instance
+     * 
+     * @return Container
+     */
     private static function container() : Container
     {
         return static::$container;
