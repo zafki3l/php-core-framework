97f8c97d1921ea53b21a488b14280f64c099e331
zafki3l <phamdinhthai2005@gmail.com>
2025-10-29 23:41:00 +0700

    add comment for index.php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,11 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
+/**
+ * Retrieve an error message from the session
+ * 
+ * @param mixed $msg
+ */
 function error($msg)
 {
     return $_SESSION['errors'][$msg];
@@ -18,11 +23,14 @@ require_once '../bootstrap/app.php';
 
 $router = new Router();
 
+// Determine the current request path relative to the project root
 $rootPath = '/' . $rootFolder;
 $path = str_replace($rootPath, '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
+// Load all route definitions from the routes directory
 foreach (glob(BASE_PATH . '/routes/*.php') as $filename) {
     require_once $filename;
 }
 
+// Dispatch the current HTTP request to the matched controller/action
 $router->dispatch($path, $_SERVER['REQUEST_METHOD']);
