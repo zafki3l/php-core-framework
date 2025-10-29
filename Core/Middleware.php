9f9c9a605f77dddae9fb762b726791123111afb7
zafki3l <phamdinhthai2005@gmail.com>
2025-10-29 23:59:52 +0700

    add comment for Middlware

--- Core/Middleware.php
+++ Core/Middleware.php
@@ -2,8 +2,17 @@
 
 namespace Core;
 
+/**
+ * Class Middlewares
+ * 
+ * provides helper methods for all Middleware Classes
+ */
 class Middleware
 {
+    /**
+     * Ensure user is authenticated
+     * @return bool
+     */
     public static function ensureAuth() : bool
     {
         return isset($_SESSION['user']) ? true : false;
