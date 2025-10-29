0cabec35241af26cefa2b24d637ea84e8ffa5c40
zafki3l <phamdinhthai2005@gmail.com>
2025-10-29 23:58:34 +0700

    add comments for errorhandler

--- Core/ErrorHandler.php
+++ Core/ErrorHandler.php
@@ -2,8 +2,18 @@
 
 namespace Core;
 
+/**
+ * Class ErrorHandler
+ * 
+ * Provides helper methods for all ErrorHandler classes
+ */
 class ErrorHandler
 {
+    /**
+     * Check is input empty
+     * @param mixed $name
+     * @return bool
+     */
     public function emptyInput($name) : bool
     {
         return empty($name);
